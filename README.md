a8a6263cc4f0d1cf147ee90121b53302f26f8752
Abdelrahman Atef <boodyatef665@gmail.com>
2021-03-08 22:40:12 +0200

    add new project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Frontend Mentor Challenges BE
+## go to website [Frontend Mentor Challenges BE]( https://frontend-mentor-projects-be.herokuapp.com/)
+<b>coded by [Boody-BE](https://github.com/Boody2004/week-days)</b>
+
+**HAVE FUN 👌**
+**Thanks, Abdelrahman-BE**
+
+---
+![Design preview for the Profile card component coding challenge](./images/desktop-preview.jpg)
---
+++ script.js
@@ -0,0 +1,38 @@
+const projects = [
+	{
+		name: 'Week Days',
+		url: 'https://week-days.herokuapp.com/',
+	},
+	{
+		name: 'Teacher Site',
+		url: 'https://teacher-site.herokuapp.com/',
+	}
+];
+
+const list = document.getElementById('list');
+
+projects.forEach(({ name, url }, i) => {
+	const listItem = document.createElement('li');
+
+	listItem.innerHTML = `
+		<a href="${url}">
+		<img src="/images/${name}/desktop-design.jpg" alt="${name}" />
+		<p>${i + 1}. ${formatProjectName(name)}</p>
+		</a>
+
+		<div class="links-container">
+			<a href="${url}" class="red">
+				<i class="fas fa-eye"></i>
+			</a>
+		</div>
+	`;
+
+	list.appendChild(listItem);
+});
+
+function formatProjectName(name) {
+	return name
+		.split('-')
+		.map(word => word[0].toUpperCase() + word.slice(1))
+		.join(' ');
+}
